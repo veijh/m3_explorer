8d8eaf0e0241fd4378fed8af00a375cd58641bf9
魏小爷 <844088636@qq.com>
2024-06-24 11:37:31 +0800

    add record script

---
+++ record_three_uav.sh
@@ -0,0 +1,4 @@
+rosbag record /merged_map \
+    /uav0/cluster_vis /uav0/mavros/local_position/odom /uav0/mavros/setpoint_raw/local /uav0/octomap_binary /uav0/view_point /uav0/frontier_normal /uav0/traj \
+    /uav1/cluster_vis /uav1/mavros/local_position/odom /uav1/mavros/setpoint_raw/local /uav1/octomap_binary /uav1/view_point /uav1/frontier_normal /uav1/traj \
+    /uav2/cluster_vis /uav2/mavros/local_position/odom /uav2/mavros/setpoint_raw/local /uav2/octomap_binary /uav2/view_point /uav2/frontier_normal /uav2/traj
